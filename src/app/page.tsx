d166c21c4e351b4fa8fad8f2e1824a221ca19a43
Vitor Franco <vitoroliveirafranco@gmail.com>
2024-12-20 10:10:34 -0300

    feat: update storage key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export interface Transaction {
   id: string
 }
 
-const storeKey = 'mensalize@transactions'
+const storeKey = '@mensalize::transactions'
 
 export default function Home() {
   const [transactions, setTransactions] = useState<Transaction[]>(() => {
